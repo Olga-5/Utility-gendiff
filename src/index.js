9e3ce2ea0fd3e382c04ffa78ba6fc3c11b0e5fc0
Olga-5 <olgasharapo2@gmail.com>
2018-08-13 22:27:57 +0300

    naming correction

--- src/index.js
+++ src/index.js
@@ -5,22 +5,22 @@ const action = [
   {
     name: 'unchangedNode',
     check: (value1, value2) => value1 === value2,
-    diff: (key, value1) => `    ${key}: ${value1}`,
+    process: (key, value1) => `    ${key}: ${value1}`,
   },
   {
     name: 'deletedNode',
     check: (value1, value2) => !value2,
-    diff: (key, value1) => `  - ${key}: ${value1}`,
+    process: (key, value1) => `  - ${key}: ${value1}`,
   },
   {
     name: 'addedNode',
     check: value1 => !value1,
-    diff: (key, value1, value2) => `  + ${key}: ${value2}`,
+    process: (key, value1, value2) => `  + ${key}: ${value2}`,
   },
   {
     name: 'changedNode',
     check: (value1, value2) => value2 !== 'undefined' && value1 !== value2,
-    diff: (key, value1, value2) => `  + ${key}: ${value2}\n  - ${key}: ${value1}`,
+    process: (key, value1, value2) => `  + ${key}: ${value2}\n  - ${key}: ${value1}`,
   },
 ];
 
@@ -29,16 +29,17 @@ const getAction = (value1, value2) => action.find(({ check }) => check(value1, v
 export default (pathToFile1, pathToFile2) => {
   const file1 = fs.readFileSync(pathToFile1, 'utf8');
   const file2 = fs.readFileSync(pathToFile2, 'utf8');
-  const contentFile1 = JSON.parse(file1);
-  const contentFile2 = JSON.parse(file2);
-  const keysFile1 = _.keys(contentFile1);
-  const keysFile2 = _.keys(contentFile2);
+  const obj1 = JSON.parse(file1);
+  const obj2 = JSON.parse(file2);
+  const keysFile1 = _.keys(obj1);
+  const keysFile2 = _.keys(obj2);
   const jointKeys = _.union(keysFile1, keysFile2);
-  const outputDiff = jointKeys.reduce((acc, key) => {
-    const valueContentFile1 = contentFile1[key];
-    const valueContentFile2 = contentFile2[key];
-    const { diff } = getAction(valueContentFile1, valueContentFile2);
-    return [...acc, diff(key, valueContentFile1, valueContentFile2)];
+  const filesDiff = jointKeys.reduce((acc, key) => {
+    const valueObj1 = obj1[key];
+    const valueObj2 = obj2[key];
+    const { process } = getAction(valueObj1, valueObj2);
+    const nodesDiff = process(key, valueObj1, valueObj2);
+    return [...acc, nodesDiff];
   }, []).join('\n');
-  return `{\n${outputDiff}\n}`;
+  return `{\n${filesDiff}\n}`;
 };
