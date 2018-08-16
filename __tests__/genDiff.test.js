3a629ca24cbf467319843e3c5cacf32947db48ce
Olga-5 <olgasharapo2@gmail.com>
2018-08-16 17:00:11 +0300

    recursive comparison

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,28 +1,52 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const pathToTestFile = './__tests__/__fixtures__/';
-const pathToResultFile = `${pathToTestFile}result.txt`;
-const result = fs.readFileSync(pathToResultFile, 'utf8');
+const pathToTestFlatFile = './__tests__/__fixtures__/flatFiles/';
+const pathToResultFlatFile = `${pathToTestFlatFile}result.txt`;
+const resultForFlatFiles = fs.readFileSync(pathToResultFlatFile, 'utf8');
 
+const pathToTestTreeFile = './__tests__/__fixtures__/treeFiles/';
+const pathToResultTreeFiles = `${pathToTestTreeFile}result.txt`;
+const resultForTreeFiles = fs.readFileSync(pathToResultTreeFiles, 'utf8');
 
-test('.json', () => {
-  const pathToFile1 = `${pathToTestFile}before.json`;
-  const pathToFile2 = `${pathToTestFile}after.json`;
+test('flat files .json', () => {
+  const pathToFile1 = `${pathToTestFlatFile}before.json`;
+  const pathToFile2 = `${pathToTestFlatFile}after.json`;
   expect(genDiff(pathToFile1, pathToFile2))
-    .toBe(result);
+    .toBe(resultForFlatFiles);
 });
 
-test('.yml', () => {
-  const pathToFile1 = `${pathToTestFile}before.yml`;
-  const pathToFile2 = `${pathToTestFile}after.yml`;
+test('flat files .yml', () => {
+  const pathToFile1 = `${pathToTestFlatFile}before.yml`;
+  const pathToFile2 = `${pathToTestFlatFile}after.yml`;
   expect(genDiff(pathToFile1, pathToFile2))
-    .toBe(result);
+    .toBe(resultForFlatFiles);
 });
 
-test('.ini', () => {
-  const pathToFile1 = `${pathToTestFile}before.ini`;
-  const pathToFile2 = `${pathToTestFile}after.ini`;
+test('flat files .ini', () => {
+  const pathToFile1 = `${pathToTestFlatFile}before.ini`;
+  const pathToFile2 = `${pathToTestFlatFile}after.ini`;
   expect(genDiff(pathToFile1, pathToFile2))
-    .toBe(result);
+    .toBe(resultForFlatFiles);
+});
+
+test('tree files .json', () => {
+  const pathToFile1 = `${pathToTestTreeFile}before.json`;
+  const pathToFile2 = `${pathToTestTreeFile}after.json`;
+  expect(genDiff(pathToFile1, pathToFile2))
+    .toBe(resultForTreeFiles);
+});
+
+test('tree files .yml', () => {
+  const pathToFile1 = `${pathToTestTreeFile}before.yml`;
+  const pathToFile2 = `${pathToTestTreeFile}after.yml`;
+  expect(genDiff(pathToFile1, pathToFile2))
+    .toBe(resultForTreeFiles);
+});
+
+test('tree files .ini', () => {
+  const pathToFile1 = `${pathToTestTreeFile}before.ini`;
+  const pathToFile2 = `${pathToTestTreeFile}after.ini`;
+  expect(genDiff(pathToFile1, pathToFile2))
+    .toBe(resultForTreeFiles);
 });
--- src/index.js
+++ src/index.js
@@ -1,48 +1,83 @@
 import _ from 'lodash';
 import fs from 'fs';
-import parse from './parsers';
+import parseFile from './parsers';
 
 
 const action = [
   {
-    name: 'unchangedNode',
+    type: 'unchangedNode',
     check: (value1, value2) => value1 === value2,
-    process: (key, value1) => `    ${key}: ${value1}`,
+    process: (key, value1, value2, indents) => `  ${indents}${key}: ${value1}`,
   },
   {
-    name: 'deletedNode',
-    check: (value1, value2) => !value2,
-    process: (key, value1) => `  - ${key}: ${value1}`,
+    type: 'deletedNode',
+    check: (value1, value2) => !value2 && !_.isBoolean(value2),
+    process: (key, value1, value2, indents) => `${indents}- ${key}: ${value1}`,
   },
   {
-    name: 'addedNode',
+    type: 'addedNode',
     check: value1 => !value1,
-    process: (key, value1, value2) => `  + ${key}: ${value2}`,
+    process: (key, value1, value2, indents) => `${indents}+ ${key}: ${value2}`,
   },
   {
-    name: 'changedNode',
+    type: 'changedNode',
     check: (value1, value2) => value2 !== 'undefined' && value1 !== value2,
-    process: (key, value1, value2) => `  + ${key}: ${value2}\n  - ${key}: ${value1}`,
+    process: (key, value1, value2, indents) => `${indents}+ ${key}: ${value2}\n${indents}- ${key}: ${value1}`,
   },
 ];
 
 
 const getAction = (value1, value2) => action.find(({ check }) => check(value1, value2));
+const getChidren = (value1, value2) => (_.isObject(value1) && _.isObject(value2)
+  ? [value1, value2] : []);
+const getProcess = typeNode => action.find(({ type }) => type === typeNode);
+
+const parseAst = (obj1, obj2) => {
+  const keysObj1 = _.keys(obj1);
+  const keysObj2 = _.keys(obj2);
+  const mergedKeys = _.union(keysObj1, keysObj2);
+  return mergedKeys.reduce((acc, key) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+    const { type } = getAction(value1, value2);
+    const children = getChidren(value1, value2);
+    const hasChildren = children.length > 0;
+    return [
+      ...acc, {
+        type: hasChildren ? 'unchangedNode' : type, key, children: parseAst(children[0], children[1]), value1, value2,
+      },
+    ];
+  }, []);
+};
+
+const stringify = (value, indents) => {
+  if (_.isObject(value)) {
+    const keys = _.keys(value);
+    const str = keys.map(key => `  ${key}: ${value[key]}`).join('\n');
+    return `{\n${indents}    ${str}\n${indents}  }`;
+  }
+  return value;
+};
+
+
+const render = (ast, indents) => ast.map((node) => {
+  const {
+    type, key, value1, value2, children,
+  } = node;
+  const { process } = getProcess(type);
+  if (children.length === 0) {
+    return process(key, stringify(value1, indents), stringify(value2, indents), indents);
+  }
+  return `  ${indents}${key}: {\n${render(children, `${indents}    `)}\n${indents}  }`;
+}).join('\n');
+
 
 export default (pathToFile1, pathToFile2) => {
   const file1 = fs.readFileSync(pathToFile1, 'utf8');
   const file2 = fs.readFileSync(pathToFile2, 'utf8');
-  const obj1 = parse(file1, pathToFile1);
-  const obj2 = parse(file2, pathToFile2);
-  const keysFile1 = _.keys(obj1);
-  const keysFile2 = _.keys(obj2);
-  const jointKeys = _.union(keysFile1, keysFile2);
-  const filesDiff = jointKeys.reduce((acc, key) => {
-    const valueObj1 = obj1[key];
-    const valueObj2 = obj2[key];
-    const { process } = getAction(valueObj1, valueObj2);
-    const nodesDiff = process(key, valueObj1, valueObj2);
-    return [...acc, nodesDiff];
-  }, []).join('\n');
-  return `{\n${filesDiff}\n}`;
+  const obj1 = parseFile(file1, pathToFile1);
+  const obj2 = parseFile(file2, pathToFile2);
+  const ast = parseAst(obj1, obj2);
+  const diff = render(ast, '  ');
+  return `{\n${diff}\n}`;
 };
